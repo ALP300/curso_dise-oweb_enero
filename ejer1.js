1ed696debcf73c11d9cb2badfe065312741d882b
ALP300 <aitorpalomino1@gmail.com>
2025-01-16 14:29:18 -0500

    Realizar ejercicio1

--- ejer1.js
+++ ejer1.js
@@ -1,1 +1,9 @@
-console.log("Hola");
+//Ejercicio 1: Imprimir los números del N al 1
+//Descripción: Pide al usuario un número N y 
+// muestra en consola los números desde N 
+// hasta 1 en orden descendente.
+
+let n= parseInt(prompt("Ingresa un número N: "));
+for(let i=n; i>=1; i--){
+    console.log(i)
+}
